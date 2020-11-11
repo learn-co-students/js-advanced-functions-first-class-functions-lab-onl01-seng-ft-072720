8dfeac2d4106d836d57031be69c7212cad3a6b74
KerriLynne <kerri.l.ogorman@gmail.com>
2020-11-11 11:34:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Code your solution in this file!
+
+// Declare a variable with const that is assigned an anonymous function. 
+// The assigned function should accept an array of drivers as an argument and 
+// return the first two drivers in the array.
+const returnFirstTwoDrivers = function (drivers) {
+    return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+};
+
+// this is an array containing two elements: the two functions that we 
+// previously defined (returnFirstTwoDrivers() and returnLastTwoDrivers()).
+
+const selectingDrivers = [ returnFirstTwoDrivers, returnLastTwoDrivers];
+
+
+// this is a higher-order function that takes in one argument, 
+// an integer, and returns a function that will multiply a fare for a ride accordingly. 
+// If createFareMultiplier() receives an argument of 4, it will return a function 
+// that takes in a fare as an argument and quadruples the fare.
+const createFareMultiplier = function (value) {
+    return function(v) {
+        return value * v;
+    };
+};
+
+// fareDoubler() — Declare a variable with const and assign a function 
+// returned by createFareMultiplier() to it. Invoke createFareMultiplier() 
+// in such a way that the new fareDoubler() function accepts a fare as its lone argument and doubles it.
+
+const fareDoubler = createFareMultiplier(2)
+
+const fareTripler = createFareMultiplier(3)
+
+// This function takes two arguments, an array of drivers and either the 
+// returnFirstTwoDrivers() or returnLastTwoDrivers() function. Based on these two arguments, 
+// selectDifferentDrivers() will return either the first two drivers or the last two drivers.
+const selectDifferentDrivers = function(drivers, diffDrivers) {
+    return diffDrivers(drivers);
+};
